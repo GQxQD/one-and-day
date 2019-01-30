51be34c6981fc1a823acacf2f0b82d6c3b9a6502
Mr-Zhizhi <591006931.qq.com>
2019-01-30 22:28:05 +0800

    update:引入scss

---
+++ one-and-day-frontend/src/views/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login-page">
+        <div class="top">
+            你好
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+.login-page{
+    
+    .top{
+
+    }
+}
+</style>
